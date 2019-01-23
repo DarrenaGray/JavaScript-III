f7a2ae787a9c28a9d002e40c907bafc1b8d4c54d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-23 18:08:36 -0500

    Completed main objectives for prototype.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global/Window Binding allows functions to be called from objects on a global or outside scope
+
+* 2. Implicit Binding restricts functions to the context of an object it is chained to
+
+* 3. New Binding uses constructor functions to create new objects to be used 
+
+* 4. Explicit Binding uses "this" to pass arguments to a function within an object
 *
 * write out a code example of each explanation above
 */
